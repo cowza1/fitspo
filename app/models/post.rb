7e60753c460c2258e1221a398efd565f504f3028
adam couser <couser1304@gmail.com>
2022-10-24 09:06:36 +0100

    updated model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :account
   has_one_attached :photo
-  has_many :comments
+  has_many :comments, , dependent: :destroy
 
   scope :active, -> { where active: true }
 end
